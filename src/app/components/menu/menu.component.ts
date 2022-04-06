480c8dde2d56d635b8e213dca15160ad71fc10ed
khq1 <74719753+khq1@users.noreply.github.com>
2022-04-06 02:32:45 +0200

    dev run save

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatMenu } from '@angular/material/menu';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.sass']
+  styleUrls: ['./menu.component.sass'],
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  title = 'ng-center-app';
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/pages/crown-page/crown-page.component.ts
+++ src/app/components/pages/crown-page/crown-page.component.ts
@@ -3,14 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-crown-page',
   templateUrl: './crown-page.component.html',
-  styleUrls: ['./crown-page.component.sass']
+  styleUrls: ['./crown-page.component.sass'],
 })
 export class CrownPageComponent implements OnInit {
-
-  constructor() { }
+  panelOpenState = false;
+  constructor() {}
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
